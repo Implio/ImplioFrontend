485ada0ce012b1fd387f470520d63d9b9ccdc728
Ani Gomez <anigomez@Anis-MacBook-Pro.local>
2018-03-31 16:57:53 -0400

    Created navbar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Nav from './nav/nav.js';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isLoggedIn: false
+		};
+	}
+
 	render() {
+		if (!this.state.isLoggedIn) {
+			return (
+				<button
+					onClick={() => {
+						this.setState({
+							isLoggedIn: true
+						});
+					}}
+				>
+					Log In
+				</button>
+			);
+		}
+
 		return (
 			<BrowserRouter>
 				<div>
+					<Nav />
 					<Switch>
-						<Route path="/" render={() => <h1>Redux Starter</h1>} />
+						<Route
+							path="/patients"
+							render={() => <h1>Patients Page</h1>}
+						/>
+
+						<Route
+							path="/dashboard"
+							render={() => <div>Dashboard</div>}
+						/>
+						<Redirect from="/" to="/dashboard" />
 					</Switch>
 				</div>
 			</BrowserRouter>
