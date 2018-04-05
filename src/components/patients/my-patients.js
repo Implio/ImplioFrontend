14ca99063207321fb66ad27287dc04d7ce1b341c
Ani Gomez <anig1496@gmail.com>
2018-04-05 16:03:10 -0400

    Add dynamic function to render patients

--- src/components/patients/my-patients.js
+++ src/components/patients/my-patients.js
@@ -3,16 +3,59 @@ import { Link } from 'react-router-dom';
 
 import PatientCard from './patient-card.js';
 
+const patientsList = [
+	{
+		_id: 0,
+		firstName: 'John',
+		lastName: 'Smith',
+		roomNumber: '123',
+		buildingNumber: '1',
+		address: '1234 West Bimbleton',
+		dob: '1997-10-05',
+		healthInsurance: {
+			companyName: 'Company',
+			memberName: 'John',
+			memberId: 'p1234567',
+			groupNumber: '123'
+		},
+		phoneNumber: '1234567',
+		picture: null,
+		documents: []
+	},
+	{
+		_id: 1,
+		firstName: 'Johnathan',
+		lastName: 'Dunn',
+		roomNumber: '123',
+		buildingNumber: '1',
+		address: '1234 West Bimbleton',
+		dob: '1997-10-05',
+		healthInsurance: {
+			companyName: 'Company',
+			memberName: 'Johnathan',
+			memberId: 'p1234567',
+			groupNumber: '123'
+		},
+		phoneNumber: '1234567',
+		picture: null,
+		documents: []
+	}
+];
+
 class MyPatients extends Component {
+	renderPatients(patients) {
+		return patients.map(patient => (
+			<PatientCard key={patient._id} patient={patient} />
+		));
+	}
+
 	render() {
 		return (
 			<section className="section">
 				<div className="container has-text-centered">
 					<h2 className="title is-2">My Patients</h2>
 
-					<PatientCard />
-					<PatientCard />
-					<PatientCard />
+					{this.renderPatients(patientsList)}
 
 					<div className="buttons is-centered create-patient">
 						<Link
--- src/components/patients/patient-card.js
+++ src/components/patients/patient-card.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logoIcon from '../../img/patient.png';
 
-class PatientCard extends Component {
-	render() {
-		return (
-			<div className="card patient-card has-text-left">
-				<div className="card-content">
-					<div className="media">
-						<div className="media-left">
-							<figure className="image is-64x64">
-								<img src={logoIcon} alt="logo" />
-							</figure>
-						</div>
-						<div className="media-content">
-							<p className="title is-5">John Smith</p>
-							<p className="subtitle is-7">P123456</p>
-						</div>
-					</div>
+const PatientCard = props => {
+	const { firstName, lastName } = props.patient;
 
-					<div className="content">
-						<p className="subtitle is-size-7">
-							<strong>Room:</strong> 111
-							<br />
-							<strong>Building:</strong> 4
-							<br />
-							<strong>Phone #:</strong> (111)-123-1234
-						</p>
+	return (
+		<div className="card patient-card has-text-left">
+			<div className="card-content">
+				<div className="media">
+					<div className="media-left">
+						<figure className="image is-64x64">
+							<img src={logoIcon} alt="logo" />
+						</figure>
+					</div>
+					<div className="media-content">
+						<p className="title is-5">{`${firstName} ${lastName}`}</p>
+						<p className="subtitle is-7">{props.patient._id}</p>
 					</div>
 				</div>
+
+				<div className="content">
+					<p className="subtitle is-size-7">
+						<strong>Room:</strong> {props.patient.roomNumber}
+						<br />
+						<strong>Building:</strong>
+						{props.patient.buildingNumber}
+						<br />
+						<strong>Phone #:</strong> {props.patient.phoneNumber}
+					</p>
+				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default PatientCard;
